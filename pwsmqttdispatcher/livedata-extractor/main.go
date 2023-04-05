3192172610907bbafb5f25a96ac663d1161ce41d
Peter Banik <peter@froggle.org>
2023-04-05 15:33:10 +0200

    Get data from PWS from its live data HTML page instead of the Wunderground update mechanism

--- pwsmqttdispatcher/livedata-extractor/main.go
+++ pwsmqttdispatcher/livedata-extractor/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
-	"os"
+	"net/http"
+	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -15,81 +18,81 @@ type Variable struct {
 	Value          string `json:"value"`
 }
 
-func main() {
+// Extract the variables from each row
+var pwsDataModel = []Variable{
+	{VarName: "", VarDescription: "", Value: ""},
+	{VarName: "", VarDescription: "", Value: ""},
+	{VarName: "", VarDescription: "", Value: ""},
+	{VarName: "", VarDescription: "", Value: ""},
+	{VarName: "", VarDescription: "", Value: ""},
+	{VarName: "", VarDescription: "", Value: ""},
+	{VarName: "", VarDescription: "", Value: ""},
+	{VarName: "", VarDescription: "", Value: ""},
+	{VarName: "receiverTime", VarDescription: "Receiver Time", Value: ""},
+	{VarName: "", VarDescription: "", Value: ""},
+	{VarName: "", VarDescription: "", Value: ""},
+	{VarName: "", VarDescription: "", Value: ""},
+	{VarName: "temperatureIndoor", VarDescription: "Indoor Temperature", Value: ""},
+	{VarName: "humidityIndoor", VarDescription: "Indoor Humidity", Value: ""},
+	{VarName: "pressureAbsolute", VarDescription: "Absolute Pressure", Value: ""},
+	{VarName: "pressureRelative", VarDescription: "Relative Pressure", Value: ""},
+	{VarName: "temperature", VarDescription: "Outdoor Temperature", Value: ""},
+	{VarName: "humidity", VarDescription: "Outdoor Humidity", Value: ""},
+	{VarName: "windDir", VarDescription: "Wind Direction", Value: ""},
+	{VarName: "windSpeed", VarDescription: "Wind Speed", Value: ""},
+	{VarName: "windGust", VarDescription: "Wind Gust", Value: ""},
+	{VarName: "solarRadiation", VarDescription: "Solar Radiation", Value: ""},
+	{VarName: "uv", VarDescription: "UV", Value: ""},
+	{VarName: "uvi", VarDescription: "UVI", Value: ""},
+	{VarName: "precipHourlyRate", VarDescription: "Hourly Rain Rate", Value: ""},
+	{VarName: "precipDaily", VarDescription: "Daily Rain", Value: ""},
+	{VarName: "precipWeekly", VarDescription: "Weekly Rain", Value: ""},
+	{VarName: "precipMonthly", VarDescription: "Monthly Rain", Value: ""},
+	{VarName: "precipYearly", VarDescription: "Yearly Rain", Value: ""},
+}
 
-	// Open the HTML file
-	file, err := os.Open("LiveData.html")
+func fetchDocumentFromPws() *goquery.Document {
+	// Read the HTML file
+	resp, err := http.Get("http://10.11.50.147/livedata.htm")
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Failed to connect to PWS: %s\n", err)
+		return nil
 	}
-	defer file.Close()
+	defer resp.Body.Close()
 
-	// // Read the HTML file
-	// resp, err := http.Get("http://example.com/table.html")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// defer resp.Body.Close()
-
-	// htmlData, err := ioutil.ReadAll(resp.Body)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	htmlData, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("Failed to fetch data from PWS: %s\n", err)
+		return nil
+	}
 
 	// Parse the HTML file with goquery
-	// doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(htmlData)))
+	doc, err := goquery.NewDocumentFromReader(strings.NewReader(string(htmlData)))
 
-	doc, err := goquery.NewDocumentFromReader(file)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Failed to process HTML: %s\n", err)
+		return nil
 	}
+	return doc
+}
 
-	// Extract the variables from each row
-	model := []Variable{
-		{VarName: "skip", VarDescription: "", Value: ""},
-		{VarName: "skip", VarDescription: "", Value: ""},
-		{VarName: "skip", VarDescription: "", Value: ""},
-		{VarName: "skip", VarDescription: "", Value: ""},
-		{VarName: "skip", VarDescription: "", Value: ""},
-		{VarName: "skip", VarDescription: "", Value: ""},
-		{VarName: "skip", VarDescription: "", Value: ""},
-		{VarName: "skip", VarDescription: "", Value: ""},
-		{VarName: "receiver_time", VarDescription: "Receiver Time", Value: ""},
-		{VarName: "skip", VarDescription: "", Value: ""},
-		{VarName: "skip", VarDescription: "", Value: ""},
-		{VarName: "skip", VarDescription: "", Value: ""},
-		{VarName: "temp_indoor", VarDescription: "Indoor Temperature", Value: ""},
-		{VarName: "humidity_indoor", VarDescription: "Indoor Humidity", Value: ""},
-		{VarName: "", VarDescription: "Absolute Pressure", Value: ""},
-		{VarName: "", VarDescription: "Relative Pressure", Value: ""},
-		{VarName: "", VarDescription: "Outdoor Temperature", Value: ""},
-		{VarName: "", VarDescription: "Outdoor Humidity", Value: ""},
-		{VarName: "", VarDescription: "Wind Direction", Value: ""},
-		{VarName: "", VarDescription: "Wind Speed", Value: ""},
-		{VarName: "", VarDescription: "Wind Gust", Value: ""},
-		{VarName: "", VarDescription: "Solar Radiation", Value: ""},
-		{VarName: "", VarDescription: "UV", Value: ""},
-		{VarName: "", VarDescription: "UVI", Value: ""},
-		{VarName: "", VarDescription: "Hourly Rain Rate", Value: ""},
-		{VarName: "", VarDescription: "Daily Rain", Value: ""},
-		{VarName: "", VarDescription: "Weekly Rain", Value: ""},
-		{VarName: "", VarDescription: "Monthly Rain", Value: ""},
-		{VarName: "", VarDescription: "Yearly Rain", Value: ""},
-	}
+func parseHtml(doc *goquery.Document) []byte {
 
 	var vars []Variable
 
 	doc.Find("table").Each(func(i int, s *goquery.Selection) {
 
-		// parse the table rows and extract the data
-		s.Find("tr").Each(func(i int, s *goquery.Selection) {
+		rows := s.Find("tr")
 
-			if i >= len(model) {
+		// parse the table rows and extract the data
+		rows.Each(func(i int, s *goquery.Selection) {
+
+			if i >= len(pwsDataModel) {
 				return
 			}
 
-			m := model[i]
-			if m.VarName == "skip" {
+			m := pwsDataModel[i]
+			if m.VarName == "" {
 				return
 			}
 
@@ -109,8 +112,25 @@ func main() {
 	// Convert the variables to JSON and print the result
 	jsonData, err := json.Marshal(vars)
 	if err != nil {
-		log.Fatal(err)
+		log.Printf("Unable to marshal JSON: %s\n", err)
+		return nil
+	}
+	return jsonData
+}
+func main() {
+
+	for {
+		doc := fetchDocumentFromPws()
+
+		if doc != nil {
+			jsonData := parseHtml(doc)
+
+			if jsonData != nil {
+				fmt.Println(string(jsonData))
+			}
+
+		}
+		time.Sleep(30 * time.Second)
 	}
 
-	fmt.Println(string(jsonData))
 }
--- pwsmqttdispatcher/src/go.mod
+++ pwsmqttdispatcher/src/go.mod
@@ -2,10 +2,14 @@ module pwsreceiver.froggle.org/pwsmqttdispatcher
 
 go 1.19
 
-require github.com/eclipse/paho.mqtt.golang v1.4.2
+require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/eclipse/paho.mqtt.golang v1.4.2
+)
 
 require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
-	golang.org/x/net v0.0.0-20200425230154-ff2c4b7c35a0 // indirect
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/net v0.7.0 // indirect
+	golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4 // indirect
 )
